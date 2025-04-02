2ddf452dc244290387461946532503abdd38e46c
Harshal292004 <malaniharshal95@gmail.com>
2025-04-02 17:15:03 +0530

    Add Vercel Analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import { ThemeProvider } from "next-themes";
+import { Analytics } from "@vercel/analytics/react"
 export const metadata: Metadata = {
   title: "Harshal Malani",
   description: "I am just a chill guy",
@@ -18,6 +19,7 @@ export default function RootLayout({
         <ThemeProvider attribute={"class"} enableSystem defaultTheme="system">
           {children}
         </ThemeProvider>
+        <Analytics></Analytics>
       </body>
     </html>
   );
